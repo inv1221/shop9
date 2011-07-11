68203a28acfdc7a9481dc864dcd5480a4c9ffcb5
Vincent <inv1221@yahoo.com>
2011-07-11 14:20:50 -0400

    store

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Shop9::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
+  get "store/index"
+
   resources :products
+  
+  
+  root :to => 'store#index', :as => 'store'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
